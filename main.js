d8a86cac204e095d9420b151059b69528df8bba9
JabWigz <johnvon2016@gmail.com>
2025-05-19 23:32:20 +0800

    Formatted Rest of the Code with Prettier & Secret Feature Added
    - Used Prettier to clean/format rest of the codes
    - Added confetti after all doses would be successfully taken by patient on webpage for 5 seconds

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ const alarmButton = document.getElementById("alarmButton");
 let lastIotAllowTime = null;
 let cooldownTimeoutId = null;
 let currentSessionId = 0;
+let confettiAnimation = 0;
 
 function fetchData() {
   updateDisplay();
@@ -115,6 +116,7 @@ async function updateDisplay() {
 
   if (newSessionId !== currentSessionId) {
     currentSessionId = newSessionId;
+    confettiAnimation = 0;
     // Clear the table
     const tableRows = document.querySelectorAll("tbody tr");
     tableRows.forEach((row, index) => {
@@ -134,6 +136,40 @@ async function updateDisplay() {
       if (doseCount == 0) {
         const olddoseString = numDoseString[1].value.split(" ");
         doseCount = olddoseString[1];
+        if (confettiAnimation == 0) {
+          confettiAnimation = 1;
+          const duration = 5 * 1000,
+            animationEnd = Date.now() + duration,
+            defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+          function randomInRange(min, max) {
+            return Math.random() * (max - min) + min;
+          }
+
+          const interval = setInterval(function () {
+            const timeLeft = animationEnd - Date.now();
+
+            if (timeLeft <= 0) {
+              return clearInterval(interval);
+            }
+
+            const particleCount = 50 * (timeLeft / duration);
+
+            // since particles fall down, start a bit higher than random
+            confetti(
+              Object.assign({}, defaults, {
+                particleCount,
+                origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+              })
+            );
+            confetti(
+              Object.assign({}, defaults, {
+                particleCount,
+                origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+              })
+            );
+          }, 250);
+        }
       }
 
       const takeDoseArrayStr = await fetchTakeDoseTime();
